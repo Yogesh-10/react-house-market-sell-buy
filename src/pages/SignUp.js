ec490bbc3109b9b7d63e7bc9df570d3eca558cad
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-04-05 21:44:40 +0530

    OAuth google

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -10,6 +10,7 @@ import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+import OAuth from '../components/OAuth';
 
 const SignUp = () => {
 	const [showPassword, setShowPassword] = useState(false);
@@ -123,6 +124,7 @@ const SignUp = () => {
 				<Link to='/sign-in' className='registerLink'>
 					Sign In Instead
 				</Link>
+				<OAuth />
 			</div>
 		</>
 	);
