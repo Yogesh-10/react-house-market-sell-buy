8b33ca7675fbdfe2cfc4c2401ee74276368afe8f
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-04-06 22:46:31 +0530

    edit and delete listings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
+reportWebVitals();
